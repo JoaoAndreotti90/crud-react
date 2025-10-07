e96c30c08559e4ffd9b20cf679a9e9047a13bb59
JoãoAndreotti90 <joaogilberto238@gmail.com>
2025-10-07 16:27:42 -0300

    Atualiza URL da API para o endereço de produção

--- frontend/src/components/template/user/User.jsx
+++ frontend/src/components/template/user/User.jsx
@@ -8,7 +8,7 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
 };
 
-const baseUrl = 'http://localhost:3001/users';
+const baseUrl = 'https://crud-react-buia.onrender.com/users';
 const initialState = {
     user: { name: '', email: '' },
     list: []
